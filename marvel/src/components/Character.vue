9970a7c2e2d238ac2381b9580f33aae33cd28c57
Zero <68662867+snagzero@users.noreply.github.com>
2022-04-02 18:23:15 -0300

    get id
    getting ids

---
+++ marvel/src/components/Character.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <div>
+        <h3>Character.vue</h3>
+        <p>{{ this.$route.params.id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Character'
+}
+        
+</script>
+
+<style lang="css">
+
+</style>
