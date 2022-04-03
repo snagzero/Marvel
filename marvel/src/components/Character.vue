91ec33ead6e5fd491cbbe2149743a20a3cdd2bdc
Zero <68662867+snagzero@users.noreply.github.com>
2022-04-02 22:22:01 -0300

    link and css

--- marvel/src/components/Character.vue
+++ marvel/src/components/Character.vue
@@ -1,22 +1,30 @@
 <template lang="html">
     <div>
-        <h3>Character.vue</h3>
-        <p>{{ this.$route.params.id}}</p>
 
-        <ul>
-            <li v-for="char in character">
-                {{char.name}}
-                {{char.description}}
-            </li>
-        </ul>
+        <div class="flex-container">
 
-        <img :src="url" alt="">
+            <div class="flex" v-for="char in character">
+                <h3>{{char.name}}</h3>
+                <p>{{char.description}}</p>
+            </div>
+
+            <div class="flex2">
+                <img :src="url" alt="" class="char-img">
+            </div>
+
+        </div>
+        <router-link to="/">
+            <button type="button" name="button" class="btn-back">Back</button>
+
+        </router-link>
+        
     </div>
 </template>
 
 <script>
 import {public_key} from '../marvel'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
     name: 'Character',
 
@@ -24,43 +32,34 @@ export default {
 
         return{
 
-            character: [],
             url: '',
-            size: 'portrait_xlarge.jpg',
+            size: 'portrait_uncanny.jpg',
         }
     },
-
     mounted(){
 
-        this.getCharacter()
+        this.$store.dispatch('getCharacter', this.$route.params.id)
+
+        this.getImage()
+
     },
+
+    computed: {
+        ...mapState({
+            character: state => state.character,
+            preUrl: state => state.url
+        })
+    },
+
 
     methods: {
 
-        getCharacter: function(){
+        getImage: function(){
 
-            var characterId = this.$route.params.id
+            this.url = `${this.preUrl}${this.size}`
 
-            axios.get(`http://gateway.marvel.com/v1/public/characters/${characterId}?apikey=${public_key}`)
-                .then((result) => {
-
-                    console.log(result)
-
-                    result.data.data.results.forEach((item) =>{
-
-                        this.character.push(item)
-
-                        this.url = `${item.thumbnail.path}/${this.size}`
-
-                        console.log(this.url)
-
-                    })
-                })
-                .catch((error) =>{
-
-                    console.log(error)
-                })
         }
+        
     }
 }
         
@@ -68,4 +67,41 @@ export default {
 
 <style lang="css">
 
+.flex-container{
+
+    margin: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.flex{
+
+    flex: 50%;
+    text-align: right;
+
+}
+
+.flex2{
+
+    flex: 50%;
+    
+}
+
+.char-img{
+
+    border-radius: 10px;
+
+}
+
+.btn-back{
+
+    width: 200px;
+    padding: 10px;
+    border-radius: 25px;
+    background-color: transparent;
+    font-size: 18px;
+    margin-bottom: 100px;
+    cursor: pointer;
+}
 </style>
--- marvel/src/components/Characters.vue
+++ marvel/src/components/Characters.vue
@@ -1,64 +1,85 @@
 <template lang="html">
     <div>
-        <h3>This is characther.vue</h3>
 
-        <ul>
+        <div class="card-container">
 
-            <li v-for="character in characters">
-                <router-link :to="{ name: 'character', params: { id: character.id } }">{{character.name}}</router-link>
-                
-            </li>
-        </ul>
+            <div class="card" v-for="character in characters">
+                <h3>{{character.name}}</h3>
+                <router-link :to="{ name: 'character', params: { id: character.id } }">
+
+                    <button type="button" name="button" class="btn-view">View</button>
+
+                </router-link>
+            </div>
+        
+        </div>
     </div>
 </template>
 
 <script>
 import { public_key, secret_key } from '../marvel';
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
     name: 'Characters',
 
-    data(){
-        
-        return{ 
-            
-            characters: []
-        }
-    },
 
     mounted(){
 
-        this.getCharacters()
+        this.$store.dispatch('getCharacters')
+        console.log(this.characters)
+        
+
+    },
+    computed: {
+
+        ...mapState({
+            characters: state => state.characters
+        })
 
     },
 
     methods:{
 
 
-        getCharacters: function(){
-    
-            axios.get(`http://gateway.marvel.com/v1/public/characters?apikey=${public_key}`)
-                .then((result) => {
-
-                    
-                    result.data.data.results.forEach((item) => {
-
-                        console.log(item)
-
-                        this.characters.push(item)
-                    })
-
-                })
-                .catch((error) => {
-
-                    console.log(error)
-                })
-      
-        }
+        
     }
 }
 </script>
 
 <style lang="css">
+
+    .card-container{
+        display: grid;
+        grid-template-columns: auto auto auto;
+        grid-gap: 10px;
+    }
+
+    .card {
+
+        text-align: center;
+        background-color: #333;
+        min-height: 280px;
+
+    }
+
+    .card h3{
+
+        color: white;
+    }
+
+    .btn-view{
+
+        padding: 10px;
+        margin-top: 50px;
+        border-radius: 15px;
+        width: 120px;
+        background-color: transparent;
+        color: white;
+        font-size: 12px;
+        font-weight: bold;
+        cursor: pointer;
+
+    }
 
 </style>
